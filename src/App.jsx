cf5305bc36de93d7a5719db68b2608c2e062c766
Azlan Jamshed <152954543+azlanjamshed@users.noreply.github.com>
2025-11-17 00:52:36 +0530

    favicon update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router";
 import Team from "./pages/Team";
